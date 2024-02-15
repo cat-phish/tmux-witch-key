a5223828d4a2e39d2be383845bd5ba8e4687e279
cat-phish <134035929+cat-phish@users.noreply.github.com>
2024-02-15 00:25:49 -0800

    maini script update

--- witch-key.sh
+++ witch-key.sh
@@ -3,9 +3,9 @@
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 tmux display-menu -T "Witch-Key" -x C -y S \
-	"Session" s "run -b 'source \"$CURRENT_DIR/witch-key-menus.sh\"; show_session_menu'" \
-	"Window" w "run -b 'source \"$CURRENT_DIR/scripts/witch-key-windows.sh\"'" \
-	"Pane" p "run -b 'source \"$CURRENT_DIR/witch-key-menus.sh\"; show_pane_menu'" \
+	"Session" s "run -b 'source \"$CURRENT_DIR/witch-key-menus.sh\" session'" \
+	"Window" w "run -b 'source \"$CURRENT_DIR/scripts/witch-key-menus.sh\"'" \
+	"Pane" p "run -b 'source \"$CURRENT_DIR/witch-key-menus.sh\"; pane'" \
 	"" \
 	"Build Menu" B "run -b 'source \"$CURRENT_DIR/witch-key-build.sh\"'" \
 	"Close Menu" Escape ""
