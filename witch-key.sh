e8b96b9de4c6dbb01a22173ea30074ab323dfe63
cat-phish <134035929+cat-phish@users.noreply.github.com>
2024-02-15 00:29:52 -0800

    main script update

--- witch-key.sh
+++ witch-key.sh
@@ -4,7 +4,7 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 tmux display-menu -T "Witch-Key" -x C -y S \
 	"Session" s "run -b 'source \"$CURRENT_DIR/witch-key-menus.sh\" session'" \
-	"Window" w "run -b 'source \"$CURRENT_DIR/scripts/witch-key-menus.sh\"'" \
+	"Window" w "run -b 'source \"$CURRENT_DIR/witch-key-menus.sh\"'" \
 	"Pane" p "run -b 'source \"$CURRENT_DIR/witch-key-menus.sh\"; pane'" \
 	"" \
 	"Build Menu" B "run -b 'source \"$CURRENT_DIR/witch-key-build.sh\"'" \
