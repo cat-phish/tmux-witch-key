744f3351f77edc82e15686bf973ea389349a23cc
cat-phish <134035929+cat-phish@users.noreply.github.com>
2024-02-14 23:07:47 -0800

    test for separate menus scripts

---
+++ scripts/with-key-windows.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR=""
+
+show_window_menu() {
+	tmux display-menu -T "Witch-Key - Windows" -x C -y S \
+		"Next Window" \' next-window \
+		"Prev Window" \; previous-window \
+		"Close Menu" Escape ""
+}
