83e1a3c38f64df263644be64808e0ffee9947666
cat-phish <134035929+cat-phish@users.noreply.github.com>
2024-02-14 22:42:27 -0800

    fix for menu calling

--- witch-key.sh
+++ witch-key.sh
@@ -4,7 +4,7 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 tmux display-menu -T "Witch-Key" -x C -y S \
 	"Session" s "run -b 'source \"$CURRENT_DIR/witch-key-menus.sh && show_session)menu\"'" \
-	"Window" w "run -b 'source \"$CURRENT_DIR/witch-key-menus.sh && show_window_menu\"'" \
+	"Window" w 'run-shell "$CURRENT_DIR/witch-key-menus.sh && show_window_menu"' \
 	"Pane" p "run -b 'source \"$CURRENT_DIR/witch-key-menus.sh && show_pane_menu\"'" \
 	"" \
 	"Build Menu" B "run -b 'source \"$CURRENT_DIR/witch-key-build.sh\"'" \
