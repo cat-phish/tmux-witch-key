6fe5ba1ee65a207b75cb9254e90a4fe3ba643064
cat-phish <134035929+cat-phish@users.noreply.github.com>
2024-02-15 00:33:44 -0800

    main script update

--- witch-key-menus.sh
+++ witch-key-menus.sh
@@ -2,6 +2,9 @@
 
 CURRENT_DIR=""
 
-tmux display-menu -T "Witch-Key - Windows" -x C -y S \
-	"Next Window" \' next-window \
-	"Close Menu" Escape ""
+show_window_menu() {
+	tmux display-menu -T "Witch-Key - Windows" -x C -y S \
+		"Next Window" \' next-window \
+		"Prev Window" \; previous-window \
+		"Close Menu" Escape ""
+}
