075256b4a035bf6740b698eb06a5476600e7baca
cat-phish <134035929+cat-phish@users.noreply.github.com>
2024-02-15 18:16:43 -0800

    testing windows menu

--- witch-key-build.sh
+++ witch-key-build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CURRENT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+#!/usr/bin/env bash
+
+CURRENT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 TMUX_KEYMAPS="$CURRENT_DIR/tmux-keymaps.txt"
 WITCH_KEY_MENUS_SH="$CURRENT_DIR/witch-key-menus.sh"
 
@@ -10,33 +13,33 @@ tmux list-keys >$TMUX_KEYMAPS
 # Start building the new witch-key-menus.sh script
 echo "#!/usr/bin/env bash" >$WITCH_KEY_MENUS_SH
 echo "" >>$WITCH_KEY_MENUS_SH
-echo "CURRENT_DIR=\"$(cd \"$(dirname \"\${BASH_SOURCE[0]}\")\" && pwd)\"" >>$WITCH_KEY_MENUS_SH
+echo "CURRENT_DIR=\"$CURRENT_DIR\"" >>$WITCH_KEY_MENUS_SH
 echo "" >>$WITCH_KEY_MENUS_SH
 echo "show_window_menu() {" >>$WITCH_KEY_MENUS_SH
 echo "    tmux display-menu -T \"Witch-Key - Windows\" -x C -y S \\" >>$WITCH_KEY_MENUS_SH
 
 # Define the commands to look for and their corresponding titles
 declare -A commands
-commands=(["next-window"]="Next Window" ["previous-window"]="Prev Window")
+commands=(["next-window"]="Next Window" ["previous-window"]="Prev Window" ["split-window -h"]="Split (H)")
 
 # Parse the tmux-keymaps.txt file
 while IFS= read -r line; do
 	if [[ $line == *"-T prefix"* ]]; then
 		bind=$(echo $line | awk '{print $5}')
-		cmd=$(echo $line | awk '{print $6}')
-		if [[ ${commands[$cmd]} ]]; then
-			title=${commands[$cmd]}
-			if [[ $bind == "\'" ]]; then
-				bind="'"
-			elif [[ $bind == "\"" ]]; then
-				bind="\""
+		for key in "${!commands[@]}"; do
+			if [[ $line == *"$key"* ]]; then
+				title=${commands[$key]}
+				if [[ $bind == "\'" ]]; then
+					bind="'"
+				elif [[ $bind == "\"" ]]; then
+					bind="\""
+				fi
+				cmd=$(echo $line | awk -v key="$key" '$0 ~ key {for(i=6;i<=NF;i++) printf $i" "; print ""}')
+				if [[ $cmd == $key* ]]; then
+					echo "        \"$title\" \"$bind\" '$cmd' \\" >>$WITCH_KEY_MENUS_SH
+				fi
 			fi
-			if [[ $line == *"-r"* ]]; then
-				echo "        \"$title\" \"$bind\" \"$cmd\" \\" >>$WITCH_KEY_MENUS_SH
-			else
-				echo "        \"$title\" \"$bind\" \"$cmd\" \\" >>$WITCH_KEY_MENUS_SH
-			fi
-		fi
+		done
 	fi
 done <$TMUX_KEYMAPS
 
--- witch-key-menus.sh
+++ witch-key-menus.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-CURRENT_DIR=""
+CURRENT_DIR="."
 
 show_window_menu() {
 	tmux display-menu -T "Witch-Key - Windows" -x C -y S \
-		"Next Window" "'" "next-window" \
-		"Prev Window" "\;" "previous-window" \
+		"Next Window" "'" 'next-window ' \
+		"Split (H)" "\\" 'split-window -h -c "#{pane_current_path}"' \
+		"Prev Window" "\;" 'previous-window ' \
 		"Close Menu" Escape ""
 }
